b1839042fafebdef4255b6a7b25f8572d847685d
yeuda <maccabibibi@gmail.com>
2021-08-30 19:49:44 +0300

    ignore emojis in app description

--- gpa.js
+++ gpa.js
@@ -1,3 +1,8 @@
+function removeEmoji(textWith) {
+    reg = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?(?:\u200d(?:[^\ud800-\udfff]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff])[\ufe0e\ufe0f]?(?:[\u0300-\u036f\ufe20-\ufe23\u20d0-\u20f0]|\ud83c[\udffb-\udfff])?)*/g
+    return textWith.replace(reg, "*");
+}
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
 
@@ -7,6 +12,9 @@ chrome.runtime.onMessage.addListener(
         let author = document.querySelector("a.hrTbp.R8zArc").textContent;
         let mail = document.querySelector(".euBY6b").textContent;
         let imgUrl = document.querySelector("img.T75of.sHb2Xb").src;
+
+        description = removeEmoji(description);
+
 
         if (request.get === "info")
 
